dd633b9268980d1faada5375dc4519aac407b17c
Carson Batt <carson.batt@bsu.edu>
2024-09-03 12:02:13 -0400

    Logic Done, Completely Untested

--- CalculatorLogic.java
+++ CalculatorLogic.java
@@ -1,2 +1,56 @@
 public class CalculatorLogic {
+    public int convertFromDecimal(int a) {
+        int mutableA = a;
+        int quotient = 1;
+        StringBuilder strA = new StringBuilder();
+        //https://www.convzone.com/decimal-to-quaternary/
+        while (quotient > 0){
+            strA.insert(0,mutableA%4);
+            mutableA = quotient;
+            quotient = Math.floorDiv(mutableA, 4);
+
+        }
+        return Integer.parseInt(String.valueOf(strA));
+    }
+
+
+    public int convertToDecimal(int a){
+        return convertToDecimalHelper(a+"");
+    }
+
+    public int convertToDecimalHelper(String a){
+        if (a.length() == 1){
+            return Integer.parseInt(a);
+        }
+        //://www.convzone.com/quaternary-to-decimal/
+        int digit = Integer.parseInt(String.valueOf(a.charAt(0)));
+        int power = (int) Math.pow(4, a.length()-1);
+        int multiplication = digit * power;
+        return multiplication + convertToDecimalHelper(a.substring(1));
+    }
+
+    public int addQuarternary(int a, int b){
+        int decimalValueA = convertToDecimal(a);
+        int decimalValueB = convertToDecimal(b);
+        int decimalSum = decimalValueA + decimalValueB;
+        int quarternarySum = convertFromDecimal(decimalSum);
+        return quarternarySum;
+    }
+
+    public int multQuarternary(int a, int b){
+        int decimalValueA = convertToDecimal(a);
+        int decimalValueB = convertToDecimal(b);
+        int decimalProduct = decimalValueA * decimalValueB;
+        int quarternaryProduct = convertFromDecimal(decimalProduct);
+        return quarternaryProduct;
+    }
+
+    public int divQuarternary(int a, int b){
+        int decimalValueA = convertToDecimal(a);
+        int decimalValueB = convertToDecimal(b);
+        int decimalQuotient = decimalValueA / decimalValueB;
+        int quarternaryQuotient = convertFromDecimal(decimalQuotient);
+        return quarternaryQuotient;
+    }
+
 }
