68f008abf348eb0ad7b6500914f586db86e5d892
Jose Rodriguez <jose.rodriguez@bsu.edu>
2024-09-06 23:44:03 -0400

    Calc frame test

--- Main/Test/QuaternaryCalculatorSE/CalculatorFrameTest.java
+++ Main/Test/QuaternaryCalculatorSE/CalculatorFrameTest.java
@@ -1,1 +1,37 @@
 package Main.Test.QuaternaryCalculatorSE;
+
+package Main.Test.QuaternaryCalculatorSE;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import Main.src.QuaternaryCalculatorSE.CalculatorDisplay;
+import Main.src.QuaternaryCalculatorSE.CalculatorFrame;
+
+import javax.swing.*;
+
+public class CalculatorFrameTest {
+
+    @Test
+    public void testCalculatorFrameSetup() {
+        CalculatorFrame frame = new CalculatorFrame();
+
+        // Check if the frame size is set correctly
+        assertEquals(1280, frame.getWidth());
+        assertEquals(720, frame.getHeight());
+
+        // Check if the display component is added to the frame
+        assertTrue(frame.getContentPane().getComponent(0) instanceof CalculatorDisplay);
+
+        @Test
+        public void testSetupFrame() {
+            CalculatorFrame frame = new CalculatorFrame();
+
+            // Check if the frame size is set correctly
+            assertEquals(1280, frame.getWidth());
+            assertEquals(720, frame.getHeight());
+
+            // check if the display component is added to the frame
+            assertTrue(frame.getContentPane().getComponent(0) instanceof CalculatorDisplay);
+        }
+    }
